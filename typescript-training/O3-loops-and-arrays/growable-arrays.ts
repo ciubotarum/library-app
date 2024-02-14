ce81b21f1382b4f5216e4500d5dc5d290618ca90
ciubotarum <ciubotarum@outlook.com>
2024-02-14 20:38:12 +0200

    Learning typescript

---
+++ typescript-training/O3-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportsTwo: string[] = ["Golf", "Cricket", "Tennis"];
+
+sportsTwo.push("Baseball");
+sportsTwo.push("Footbal");
+
+for (let tempSport of sportsTwo) {
+    console.log(tempSport);
+}

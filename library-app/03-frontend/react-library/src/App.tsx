606e89bedadae139987b959526e65728e05ef14b
ciubotarum <ciubotarum@outlook.com>
2024-03-02 12:47:29 +0200

    Book Checkout Page

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -4,20 +4,25 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 
 export const App = () => {
   return (
-    <>
-      <Navbar />
+    <div className='d-flex flex-column min-vh-100'>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path='/search' element={<SearchBooksPage />} />
-        </Routes>
+        <Navbar />
+        <div className='flex-grow-1'>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path='/search' element={<SearchBooksPage />} />
+            <Route path='/checkout/:bookId' element={<BookCheckoutPage/>}/>
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
-      <Footer />
-    </>
+    </div>
   );
-};
+}
 
---
+++ library-app/03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+
+export const BookCheckoutPage = () => {
+
+    const [book, setBook] = useState<BookModel>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    const bookId = (window.location.pathname).split('/')[2];
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+            const response = await fetch(baseUrl);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+        };
+        fetchBook().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+    
+    return (
+        <div>
+            <h3>Hi World!</h3>
+        </div>
+    );
+}

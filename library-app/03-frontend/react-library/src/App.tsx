847cdb18f1b1472a2f72d38f232018df71438e97
ciubotarum <ciubotarum@outlook.com>
2024-03-05 18:20:41 +0200

    Okta Sign In

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -4,24 +4,47 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useNavigate} from "react-router-dom";
+
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { oktaConfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { Security } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWidget';
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+
+  const customAuthHandler = () => {
+    navigate('/login');
+  }
+
+  const navigate = useNavigate();
+  const LoginWidgetWrapper = () => <LoginWidget config={oktaConfig} />
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    navigate(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
   return (
     <div className='d-flex flex-column min-vh-100'>
-      <BrowserRouter>
-        <Navbar />
-        <div className='flex-grow-1'>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path='/search' element={<SearchBooksPage />} />
-            <Route path='/checkout/:bookId' element={<BookCheckoutPage/>}/>
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+        <BrowserRouter>
+          <Navbar />
+          <div className='flex-grow-1'>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path='/search' element={<SearchBooksPage />} />
+              <Route path='/checkout/:bookId' element={<BookCheckoutPage />} />
+              {/* <Route path='/login' element={<LoginWidget config={oktaConfig} />} /> */}
+              <Route path='/login' element={<LoginWidgetWrapper />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </Security>
     </div>
   );
 }

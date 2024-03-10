aa1a13d7026395fcf9ac2b9eddb12c1988ef9f06
ciubotarum <ciubotarum@outlook.com>
2024-03-10 12:49:15 +0200

    Shelf Page

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -4,14 +4,16 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { BrowserRouter, Navigate, Route, Routes, useNavigate} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { Security } from '@okta/okta-react';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewLIstPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
+import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
+
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -29,8 +31,8 @@ export const App = () => {
 
   return (
     <div className='d-flex flex-column min-vh-100'>
-      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} >
         <BrowserRouter>
+        <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} >
           <Navbar />
           <div className='flex-grow-1'>
             <Routes>
@@ -40,12 +42,16 @@ export const App = () => {
               <Route path='/reviewlist/:bookId' element={<ReviewLIstPage />} />
               <Route path='/checkout/:bookId' element={<BookCheckoutPage />} />
               <Route path='/login' element={<LoginWidget config={oktaConfig} />} />
+              <Route path='/login/callback' element={<LoginCallback />} />
+              <SecureRoute path='/shelf' element={<ShelfPage/>}/>
             </Routes>
           </div>
+          {/* <SecureRoute path='/shelf' element={<ShelfPage/>}>  </SecureRoute> */}
           <Footer />
+          </Security>
         </BrowserRouter>
-      </Security>
     </div>
   );
+
 }
 
